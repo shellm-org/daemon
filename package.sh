c70ca8d36bf3129e81ae904d6f42ab4a8a796753
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-07-02 20:00:25 +0200

    Update shellman location and deps

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 BINS="bin/daemon"  #:bin/other-script
 LIBS="lib/daemon.sh"
-DEPS=shellm/shellm-shellman  #:user2/repo2
+DEPS=gitlab.com/shellm/shellman  #:user2/repo2
 BASH_COMPLETIONS="cmp/daemon.completion.bash"
 ZSH_COMPLETIONS="cmp/daemon.completion.zsh"
